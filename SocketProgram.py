5ff27d43e39b480332c719eef01c5a1bdc516deb
lukevukovich <lukevuke83@gmail.com>
2024-09-29 10:28:22 -0400

    Write control loop to handle client-server communication

--- SocketProgram.py
+++ SocketProgram.py
@@ -8,7 +8,7 @@ def get_port() -> int:
     """
     good_port = False
     while not good_port:
-        port = input(f"Enter the port number for host {HOST} (0 to exit): ")
+        port = input(f"Enter port number for host {HOST} (0 to exit): ")
         try:
             port = int(port)
             if 0 <= port <= 65535:
@@ -20,12 +20,42 @@ def get_port() -> int:
 
     return port
 
+def get_client_input() -> str:
+    """
+    Get the code and message from client in format: {code}#{message}. 0 will exit the program. Code must be 1 or 2.
+    """
+    good_input = False
+    while not good_input:
+        client_input = input("Client Input (0 to exit): ")
+        if client_input == "":
+            print("Input cannot be blank\n")
+        elif client_input == "0":
+            return 0, None
+        else:
+            try:
+                code_message = client_input.split("#")
+                code = code_message[0]
+                message = code_message[1]
+                if code != "1" and code != "2":
+                    print("Invalid code\n")
+                else:
+                    if message == "":
+                        print("Message cannot be blank\n")
+                    else:
+                        good_input = True
+                
+            except Exception:
+                print("Invalid Input: Must be in format: {code}#{message}\n")
+
+    return code, message
+
 def main():
     """
-    Main function to connect to the server.
+    Main function to handle client-server communication. Client will connect and send messages to the server.
     """
     # Create a socket object
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket Created")
 
     # Get the port number
     port = get_port()
@@ -34,13 +64,35 @@ def main():
         return
     
     try:
-        # connect to the server on local computer
+        # Client connects to the server
         client_socket.connect((HOST, port))
 
-        # receive data from the server
-        print(f"Server: {client_socket.recv(1024).decode()}")
+        # Receive initial message from the server
+        print(f"Server Output: {client_socket.recv(1024).decode()}")
+
+        # Control loop for client-server communication
+        exit = False
+        while not exit:
+            # Get code and message from client
+            code, message = get_client_input()
+
+            # Exit program if code is 0
+            if code == 0:
+                print("Exit Program")
+                exit = True
+                continue
+
+            # Send message to server
+            client_socket.sendall(message.encode())
+            print(f"Message Received by Server")
+            if code == "1":
+                # Echo message from server if code is 1
+                print(f"Server Output: {client_socket.recv(1024).decode()}\n")
+            else:
+                print("")
 
     except Exception as e:
+        # Handle any errors that occur
         print(f"Socket Program Error: {e}")
 
     finally:
